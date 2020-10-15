3013967b41a7e7b02a8e63373e86e5be547683aa
nazeudon <nazeudon48112@icloud.com>
2020-10-15 09:15:04 +0900

    some interpolations

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
+//裏でcombineReducerが働いて、個々のReducerを結合している
 export default configureStore({
   reducer: {
     counter: counterReducer,
