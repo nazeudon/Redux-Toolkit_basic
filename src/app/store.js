0262489a7eeedf9e8d928577a4267cb5d2c81da3
nazeudon <nazeudon48112@icloud.com>
2020-10-15 09:56:00 +0900

    02_basic_create_slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import taskReducer from "../features/task/taskSlice";
 
 //裏でcombineReducerが働いて、個々のReducerを結合している
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    task: taskReducer,
   },
 });
---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const taskSlice = createSlice({
+  name: "task",
+  initialState: {
+    idCount: 3,
+    tasks: [
+      { id: 3, title: "Task C", completed: false },
+      { id: 2, title: "Task B", completed: true },
+      { id: 1, title: "Task A", completed: false },
+    ],
+  },
+  reducers: {
+    newTask: (state, action) => {
+      state.idCount += 1;
+      const newItem = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [newItem, ...state.tasks];
+    },
+    completeTask: (state, action) => {
+      const task = state.tasks.find((t) => t.id === action.payload.id);
+      if (task) {
+        task.completed = !task.completed;
+      }
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((t) => t.id !== action.payload.id);
+    },
+  },
+});
+
+export const { newItem, completeTask, deleteTask } = taskSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state) => state.counter.value)`
+export const selectTasks = (state) => state.task.tasks;
+
+export default taskSlice.reducer;
