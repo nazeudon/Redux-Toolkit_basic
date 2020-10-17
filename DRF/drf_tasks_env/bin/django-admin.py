ba4b5430bac6480f0a5e7ba2e92ae6daa806b34c
nazeudon <nazeudon48112@icloud.com>
2020-10-17 09:32:32 +0900

    06_practial_create_django_project

---
+++ DRF/drf_tasks_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hnakatani/Documents/その他/Learning/JavaScript/Udemy/redux-tool-kit_practical/DRF/drf_tasks_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
